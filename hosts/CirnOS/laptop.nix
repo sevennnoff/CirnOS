851ff6dd9636086efaf325d8d6d181e13a3011c2
sevennnoff <off@sevennn.ru>
2025-04-26 14:49:20 +0300

    Слава AMD

--- hosts/CirnOS/laptop.nix
+++ hosts/CirnOS/laptop.nix
@@ -20,11 +20,11 @@
     };
 
   # nvidia
-  hardware.opengl = {
-    enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
-  };
+  #hardware.opengl = {
+  #  enable = true;
+  #  driSupport = true;
+  #  driSupport32Bit = true;
+  #};
 
   # services.xserver.videoDrivers = [ "nvidia" ];
 
